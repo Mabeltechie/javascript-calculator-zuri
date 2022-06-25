54eacdd4ca1160f77f3dd88c3a2cf60304d3fc7a
Mabel Nwanze <103879042+Mabeltechie@users.noreply.github.com>
2022-06-25 20:57:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var num1 = prompt('num1:')
+var opr = parseInt(prompt('operator: - / * +'))
+var num2 = parseInt(prompt('number 2:'))
+
+if (opr ==='+') {alert(num1+ num2)
+     console.log (num1+ num2)}
+ else if (opr ==='+'){alert(num1 + num2);
+    console.log (num1 + num2)}
+ else if (opr ==='-'){alert(num1 - num2);
+        console.log (num1 - num2)}
+ else if (opr ==='*'){alert(num1 * num2);
+            console.log (num1 * num2)}
+ else if (opr ==='/'){alert(num1 / num2);
+                console.log (num1 / num2)}
+                
+            
